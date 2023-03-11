a0f210e529f01306c42fd42cbda349715e41f5f6
mertSezerO <117273151+mertSezerO@users.noreply.github.com>
2023-03-11 18:15:32 +0300

    Authentication operations done.

--- server.js
+++ server.js
@@ -1,12 +1,97 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
+
+const users = []
 
 app.set('view-engine', 'ejs')
-
+app.use(express.urlencoded({ extended: false }))
 app.use('/public', express.static('public'))
+app.use(flash())
+app.use(session({
+  secret: 'somevalue',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-    res.render('index.ejs')
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
 })
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('loggedin.ejs', { name: req.user.name })
+  })
+  
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.post('/register', async (req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.forEach(user => {
+            if(user.username === req.body.username){
+                throw Error("this username is taken")
+            }
+            else if(user.email === req.body.username){
+                throw Error("there exists already an account with this email")
+            }
+        });
+        users.push({
+            id: Date.now.toString(),
+            name: req.body.name,
+            surname: req.body.surname,
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/')
+    } catch(err){
+        res.redirect('/register')
+        console.error(err.message)
+    }
+    console.log(users)
+})
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+  
+    res.redirect('/login')
+}
+  
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return res.redirect('/')
+    }
+    next()
+}
 
 app.listen(3000)
